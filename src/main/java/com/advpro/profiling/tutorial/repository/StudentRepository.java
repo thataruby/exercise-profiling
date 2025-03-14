708323945be549ffb71dec8469c984fa79fc9abf
thataruby <thata@dfi.id>
2025-03-14 15:03:51 +0700

    [Refactoring] - Optimizing method findStudentWithHighestGpa

--- src/main/java/com/advpro/profiling/tutorial/repository/StudentRepository.java
+++ src/main/java/com/advpro/profiling/tutorial/repository/StudentRepository.java
@@ -4,6 +4,7 @@ import com.advpro.profiling.tutorial.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
+import java.util.Optional;
 
 /**
  * @author muhammad.khadafi
@@ -12,4 +13,7 @@ import org.springframework.data.jpa.repository.Query;
 public interface StudentRepository extends JpaRepository<Student, Long> {
     @Query("SELECT STRING_AGG(s.name, ', ') FROM Student s")
     String joinStudentNames();
+
+    @Query("SELECT s FROM Student s ORDER BY s.gpa DESC LIMIT 1")
+    Optional<Student> findStudentWithHighestGpa();
 }
